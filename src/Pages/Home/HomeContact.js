16b34a4c0eb72695276561cde958b2708e7067e3
saddamhosan <contact.saddamhosan.com>
2022-05-10 19:34:30 +0600

    added style

--- src/Pages/Home/HomeContact.js
+++ src/Pages/Home/HomeContact.js
@@ -3,33 +3,39 @@ import bg from '../../assets/images/appointment.png';
 
 const HomeContact = () => {
     return (
-      <div style={{ background: `url(${bg})`, backgroundSize: "cover" }} className='my-20'>
+      <div
+        style={{ background: `url(${bg})`, backgroundSize: "cover" }}
+        className="my-20"
+      >
         <div className="text-center py-10">
-            
-          <h4 className='text-secondary text-xl'>Contact Us</h4>
-          <h1 className='text-3xl text-white'>Stay Connected with us</h1>
+          <h4 className="text-secondary text-xl">Contact Us</h4>
+          <h1 className="text-3xl text-white">Stay Connected with us</h1>
           <form>
             <input
-              className="w-72 my-3 text-xl p-2 rounded-xl"
+              className="lg:w-1/2 w-10/12 my-3 text-xl p-2 rounded-xl"
               type="email"
               name="email"
               placeholder="Email Address"
             />
             <br />
             <input
-              className="w-72 my-3 text-xl p-2 rounded-xl"
+              className="lg:w-1/2 w-10/12 my-3 text-xl p-2 rounded-xl"
               type="text"
               name="subject"
               placeholder="Subject"
             />
             <br />
             <textarea
-              className="w-72 my-3 text-xl p-2 rounded-xl"
+              className="lg:w-1/2 w-10/12 my-3 text-xl p-2 rounded-xl"
               name="message"
               placeholder="Your message"
             ></textarea>
             <br />
-            <input className='text-xl text-white bg-gradient-to-r from-secondary to-primary px-6 py-2 rounded-xl font-bold' type="submit" value="Submit" />
+            <input
+              className="text-xl text-white bg-gradient-to-r from-secondary to-primary px-6 py-2 rounded-xl font-bold"
+              type="submit"
+              value="Submit"
+            />
           </form>
         </div>
       </div>
